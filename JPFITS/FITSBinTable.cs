b60437aaab3d3a1c3d860774bf4ed8cdd46f5058
Joseph E Postma <joepostma@live.ca>
2023-09-14 07:42:19 -0600

    2023.09.14

--- JPFITS/FITSBinTable.cs
+++ JPFITS/FITSBinTable.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*  
+    JPFITS: object-oriented FITS file interaction
+    Copyright (C) 2023  Joseph E. Postma
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	joepostma@live.ca
+*/
+
+using System;
 using System.Collections;
 using System.Threading.Tasks;
 using System.IO;
